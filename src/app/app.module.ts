1474ff8958506f34186172cdade2f65085a03b7d
Alexander <car3651@yandex.ru>
2020-12-12 01:44:16 +0500

    added filtration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormatInterceptor } from './format.interceptor';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { FormatInterceptor } from './format.interceptor';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule 
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCheckboxModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: FormatInterceptor, multi: true}],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -9,12 +9,27 @@ import { RestClientService } from '../rest-client.service';
 })
 export class TableComponent implements OnInit {
   users:User[] = [];
+  filteredUsers:User[] = [];
   constructor(private restClient:RestClientService) { }
 
   ngOnInit(): void {
     this.restClient.getUsers().subscribe(response => {
       this.users = response;
+      this.filteredUsers = this.users;
     })
   }
 
+  updateTable() {
+
+  }
+
+  filter(selUsers: string[]): void {
+    console.log(selUsers)
+    this.filteredUsers = new Array<User>();
+    for (const user of this.users) {
+      if (selUsers.includes(user.name) || selUsers.includes(user.email) || selUsers.includes(user.address)) {//заготовка алгоритма фильтрации
+        this.filteredUsers.push(user);
+      }
+    }
+  }
 }
